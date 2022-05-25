0bfc7e2ea439e1b85483bb4271a30af234f3e586
Guilherme Zacarias Beloni <97005507+GuilhermeBeloni@users.noreply.github.com>
2022-05-25 18:38:39 -0300

    .php

---
+++ ProjetoPortifolio/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Meu Portifólio</title>
+  </head>
+  <body>
+    <header class="header-wrapper">
+      <div class="checkbox-container">
+        <div class="checkbox-wrapper">
+          <input type="checkbox" id="toggle" />
+          <label class="checkbox" for="toggle">
+            <div class="trace"></div>
+            <div class="trace"></div>
+            <div class="trace"></div>
+          </label>
+          <div class="menu"></div>
+          <nav class="menu-items">
+            <ul>
+              <li>
+                <a href="index.html">Home</a>
+              </li>
+              <li>
+                <a href="#">Sobre</a>
+              </li>
+              <li>
+                <a href="#">Projetos</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <h1>Guilherme Zacarias Beloni</h1>
+      <h2>Front-end developer</h2>
+      <div class="social-media">
+        <a
+          href="https://www.linkedin.com/in/guilherme-beloni-714216b7/"
+          target="_blank"
+          >LinkedIn</a
+        >
+        <a href="https://github.com/GuilhermeBeloni" target="_blank">GitHub</a>
+      </div>
+    </header>
+    <main class="container">
+      <div class="card-container">
+        <div class="card-text">
+          Aqui estão todos os projetos estudados ao longo da minha jornada como estudante de Análise e Desenvolvimento de Sistemas. Linguagem Python, HTML5, CSS3, REACT, SQL, UX/UI, Sistemas Operacionais, dentre outros! 
+        </div>
+        <div class="card">
+            <div class="card-wrapper">
+                <h2> GitHub</h2>
+                <p>Veja os meus projetos</p>
+            </div>
+        </div>
+      </div>
+    </main>
+    <footer class="footer">
+        Guilherme Beloni. Todos os direitos reservados.
+    </footer>
+  </body>
+</html>
